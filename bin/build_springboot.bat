1d1c17d576a8a51a2a4e07594444492200ff3aad
InverseQuicksands <1602629971@qq.com>
2019-07-20 18:03:06 +0800

    delete other script

--- bin/build_springboot.bat
+++
@@ -1,5 +0,0 @@
-@rem 编译springboot项目打包命令
-
-@rem From Spring Boot release 2.0 M1 the bootRepackage task has been replaced with bootJar and bootWar tasks
-@rem %QUICKSAND_SDK_HOME%/gradlew.bat clean build -x test -x bootRepackage
-@gradlew.bat clean build -x test -x bootJar
--- bin/deploy_springboot.bat
+++
@@ -1,2 +0,0 @@
-@rem 上传项目jar包至maven仓库
-%QUICKSAND_SDK_HOME%/gradlew.bat clean build uploadArchives -x test -x bootJar
